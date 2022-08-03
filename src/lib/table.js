b838b81cb4c359692e80e8cbac20f2c9030de242
Eric Mockler <emockler@ucsc.edu>
2022-08-03 00:09:59 -0700

    deserialize table view model

--- src/lib/table.js
+++ src/lib/table.js
@@ -8,11 +8,9 @@ export class Table {
 
 		this.headerRows;
 		this.rows;
+		this.originalRows;
 		this.currentPageRows;
 
-		this.columns;
-		this.columnDefinitionList;
-		this.columnGroupList;
 		this.visibleColumns;
 
 		this.pluginStates;
@@ -28,15 +26,15 @@ export class Table {
 
 		this.instance = createTable(dataList, pluginMap);
 
+		let columns;
 		if (columnGroupList <= 0) {
-			this.columns = this.instance.createColumns(
+			columns = this.instance.createColumns(
 				columnDefinitionList.map(({ name, id, pluginOptions }) => {
 					return this.instance.column({ header: name, accessor: id, plugins: pluginOptions });
 				})
 			);
 		} else {
-			// TODO: create column groups
-			this.columns = this.instance.createColumns(
+			columns = this.instance.createColumns(
 				columnGroupList.map((groupName) => {
 					return this.instance.group({
 						header: groupName,
@@ -49,5 +47,20 @@ export class Table {
 				})
 			);
 		}
+
+		const viewModel = this.instance.createViewModel(columns);
+
+		this.attributes = viewModel.tableAttrs;
+		this.headerAttributes = viewModel.tableHeadAttrs;
+		this.rowAttributes = viewModel.tableBodyAttrs;
+
+		this.headerRows = viewModel.headerRows;
+		this.rows = viewModel.rows;
+		this.originalRows = viewModel.originalRows;
+		this.currentPageRows = viewModel.pageRows;
+
+		this.visibleColumns = viewModel.visibleColumns;
+
+		this.pluginStates = viewModel.pluginStates;
 	}
 }
