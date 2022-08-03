4c33395b237dd96d059c94bd343913005e41dade
Eric Mockler <emockler@ucsc.edu>
2022-08-02 23:52:37 -0700

    sync table builder & gql schemas reflect database

--- src/lib/table.js
+++ src/lib/table.js
@@ -1,9 +1,53 @@
-import { writable } from 'svelte/store';
-
 export class Table {
-	constructor(
-		valueMap = writable([{ header: '', value: '' }]),
-		columns = writable([{ header: '', accessor: '', pluginOptions: {} }]),
-		plugins = {}
-	) {}
+	constructor() {
+		this.instance;
+
+		this.attributes;
+		this.headerAttributes;
+		this.rowAttributes;
+
+		this.headerRows;
+		this.rows;
+		this.currentPageRows;
+
+		this.columns;
+		this.columnDefinitionList;
+		this.columnGroupList;
+		this.visibleColumns;
+
+		this.pluginStates;
+	}
+
+	build(
+		dataList = [],
+		pluginMap = {},
+		columnDefinitionList = [{ name: '', id: '', pluginOptions: {}, group: null }],
+		columnGroupList = []
+	) {
+		import { createTable } from 'svelte-headless-table';
+
+		this.instance = createTable(dataList, pluginMap);
+
+		if (columnGroupList <= 0) {
+			this.columns = this.instance.createColumns(
+				columnDefinitionList.map(({ name, id, pluginOptions }) => {
+					return this.instance.column({ header: name, accessor: id, plugins: pluginOptions });
+				})
+			);
+		} else {
+			// TODO: create column groups
+			this.columns = this.instance.createColumns(
+				columnGroupList.map((groupName) => {
+					return this.instance.group({
+						header: groupName,
+						columns: columnDefinitionList
+							.filter((definition) => definition.group === groupName)
+							.map(({ name, id, pluginOptions }) => {
+								return this.instance.column({ header: name, accessor: id, plugins: pluginOptions });
+							})
+					});
+				})
+			);
+		}
+	}
 }
